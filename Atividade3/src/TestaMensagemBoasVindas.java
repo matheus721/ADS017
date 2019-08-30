eef1afa9d5f30387ffd91a6d30d3d1fa7d1c7be2
18214290009 <18214290009@OLAB04-D12.academico.iesb.br>
2019-08-29 22:45:23 -0300

    atividade3

---
+++ Atividade3/src/TestaMensagemBoasVindas.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+
+
+public class TestaMensagemBoasVindas {
+    public static void main(String [] args) {
+        JOptionPane option = new JOptionPane();
+        
+        System.out.println("Deseja iniciar o programa?");
+        int YES_OPTION = JOptionPane.YES_OPTION;
+    }
+}
