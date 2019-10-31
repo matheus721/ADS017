0804c8bd1097635076ef1786f2d2bfa8b976857a
18214290009 <18214290009@OLAB04-D12.academico.iesb.br>
2019-10-31 20:25:36 -0300

    Atividade12

--- Atividade12/src/FormularioTabela.java
+++ Atividade12/src/FormularioTabela.java
@@ -1,29 +1,78 @@
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 
 public class FormularioTabela extends JFrame{
-    public   FormularioTabela() {
-    JTextField campoFiltro = new JTextField();
+
+    private JTextField campoFiltro;
+    private JButton botaoFiltrar;
+    private JButton botaoLimpar;
+    private JTable tabela;
+    private String[][] arrayDados;
+    private String[] arrayColunas;
+    private JLabel rotuloFiltro;
     
-    JButton botaoFiltrar = new JButton("Filtrar");
-    JButton botaoLimpar = new JButton("Limpar");
-    
-    JTable tabela = new JTable();
-    
-    String[][] arrayDados = {
-        {"2015001","João","joao@iesb.br"},
-        {"2015002","Ana","ana@iesb.com"},
-        {"2015003","Pedro","pedro@iesb.com"},
-        {"2015004","Bianca","bianca@iesb.com"},
-        {"2015005","maria","maria@iesb.com"}
+    public FormularioTabela()  throws HeadlessException{
+      arrayDados = new String[][]{
+         {"2015001","João","joao@iesb.br"},
+         {"2015002","Ana","ana@iesb.com"},
+         {"2015003","Pedro","pedro@iesb.com"},
+         {"2015004","Bianca","bianca@iesb.com"},
+         {"2015005","maria","maria@iesb.com"}
     };
-    String[] arrayColunas = {"Matrícula","Nome","E-mail"};
+    arrayColunas = new String[] {"Matricula", "Nome", "E-mail"};
+    botaoFiltrar = new JButton("Filtrar");
+    botaoLimpar = new JButton("limpar");
+    campoFiltro = new JTextField(20);
+    rotuloFiltro = new JLabel("informe um nome:");
+    DefaultTableModel modelo = new DefaultTableModel(arrayDados, arrayColunas);
+    tabela = new JTable(modelo);
+    JScrollPane rolagem = new JScrollPane();
+    rolagem.setViewportView(tabela);
+    JPanel painel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    painel.add(rotuloFiltro);
+    painel.add(campoFiltro);
+    painel.add(botaoFiltrar);
+    painel.add(botaoLimpar);
     
-    JLabel rotulo =new JLabel("Informe um nome:");
-  }   
+    botaoLimpar.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            DefaultTableModel  modelo = new DefaultTableModel(arrayDados, arrayColunas);
+            tabela.setModel(modelo);
+          }
+      });
+    
+    botaoFiltrar.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+              modelo.setNumRows(0);
+              for (String[] linha : arrayDados){
+                  if (linha[1].contains(campoFiltro.getText())){
+                      modelo.addRow(linha);
+                  }
+              }
+          }
+      });
+    
+    add(painel, BorderLayout.NORTH);
+    add(rolagem);
+    setSize(640,480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+}
+    
 }
